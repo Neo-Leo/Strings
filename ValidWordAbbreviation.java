38ab2b44c3f5e0707dce5d4dd4d9f8bc93056738
Neo-Leo <sharm398@umn.edu>
2017-01-04 04:02:40 -0600

    New code with O(m+n) time complexity

--- ValidWordAbbreviation.java
+++ ValidWordAbbreviation.java
@@ -3,37 +3,34 @@
  * https://leetcode.com/problems/valid-word-abbreviation/
  */ 
 
-public class ValidWordAbbreviation {
+public class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
-        if(abbr == null || abbr.length() == 0) return false; 
-        int n=word.length(), zeroCount=0; 
-        int bitMask = (int)Math.pow(2,n)-1, one=1, i=0; 
-        while(bitMask >= 0){
-            StringBuilder sb = new StringBuilder(); 
-            int tempMask=bitMask;
-            zeroCount = 0; 
-            i = 0;
-            while(i < n) {
-                if((tempMask&one) != 0) {
-                    if(zeroCount != 0) {
-                        sb.append(zeroCount);
-                        zeroCount = 0; 
-                    }
-                    sb.append(word.charAt(i));
-                } else {
-                    zeroCount++; 
+        if(word == null && abbr == null) return true; 
+        if(word == null || abbr == null) return false; 
+        if(word.length()==0 && abbr.length()==0) return true; 
+        if(word.length()==0 || abbr.length()==0) return false; 
+        int i=0,j=0;
+        while(i<=word.length()-1 && j <= abbr.length()-1){
+            char ch = abbr.charAt(j); 
+            if(!Character.isDigit(ch)){
+                if(ch != word.charAt(i)) return false; 
+                j++; 
+                i++; 
+            } else {
+                int skip = 0; 
+                if(j<=abbr.length()-1 && ch == '0') return false; 
+                while(j<=abbr.length()-1 && Character.isDigit(abbr.charAt(j))){
+                    skip = skip*10 + abbr.charAt(j)-'0'; 
+                    j++; 
+                } 
+                if(skip > word.length()-i) return false; 
+                while(skip>0){
+                    i++; 
+                    skip--; 
                 }
-                i++;
-                tempMask=tempMask>>1; 
-            }
-            
-            if(zeroCount != 0) {
-                sb.append(zeroCount);
-            }
-            String s = new String(sb);
-            if(s.equals(abbr)) return true; 
-            bitMask--; 
+            } 
         }
+        if(i == word.length() && j == abbr.length()) return true; 
         return false; 
     }
 }
